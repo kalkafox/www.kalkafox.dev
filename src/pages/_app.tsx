f64f4e939356749b9c2d9508e06e60ee60968179
Kalka <kalka2088@gmail.com>
2023-04-26 21:42:40 -0400

    move meta to app root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import Head from 'next/head'
+
+import meta from '@/data/meta.json'
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
@@ -8,6 +12,43 @@ const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#597056' />
+        <meta name='msapplication-TileColor' content='#79BC71' />
+        <meta name='theme-color' content='#79BC71' />
+        <meta name='description' content={meta.description} />
+        <meta property='og:title' content={meta.title} />
+        <meta property='og:description' content={meta.description} />
+        <meta property='og:image' content={meta.image} />
+        <meta property='og:url' content={meta.url} />
+        <meta property='og:type' content='website' />
+        <meta property='og:site_name' content='kalkafox' />
+        <meta property='og:locale' content='en_US' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:site' content='@kalkafox' />
+        <meta name='twitter:creator' content='@kalkafox' />
+        <meta name='twitter:title' content={meta.title} />
+        <meta name='twitter:description' content={meta.description} />
+        <meta name='twitter:image' content={meta.image} />
+      </Head>
       <Component {...pageProps} />
     </QueryClientProvider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,9 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-
-import meta from '@/data/meta.json'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/site.webmanifest' />
-        <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#597056' />
-        <meta name='msapplication-TileColor' content='#79BC71' />
-        <meta name='theme-color' content='#79BC71' />
-        <meta name='description' content={meta.description} />
-        <meta property='og:title' content={meta.title} />
-        <meta property='og:description' content={meta.description} />
-        <meta property='og:image' content={meta.image} />
-        <meta property='og:url' content={meta.url} />
-        <meta property='og:type' content='website' />
-        <meta property='og:site_name' content='kalkafox' />
-        <meta property='og:locale' content='en_US' />
-        <meta name='twitter:card' content='summary_large_image' />
-        <meta name='twitter:site' content='@kalkafox' />
-        <meta name='twitter:creator' content='@kalkafox' />
-        <meta name='twitter:title' content={meta.title} />
-        <meta name='twitter:description' content={meta.description} />
-        <meta name='twitter:image' content={meta.image} />
-      </Head>
       <main className={`font-['Times_New_Roman']`}>
         Whoa! You&apos;re on the lo-fi version of https://www.kalkafox.dev/
         <br />
